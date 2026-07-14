f51a9efb625a48a8d59c386d4dc84ae49b66b19e
ananya-singh-23 <ananyasingh8763@gmail.com>
2026-07-14 19:52:02 +0530

    adding different pges

---
+++ src/pages/adding.jsx
@@ -0,0 +1,41 @@
+import { getDatabase, ref, set } from "firebase/database";
+import React from "react";
+import { app } from "../firebase";
+
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { Route, Routes } from "react-router-dom";
+
+
+const db = getDatabase(app);
+const auth = getAuth(app);
+
+
+const Adding = () => {
+    const handleSignup = () => {
+    createUserWithEmailAndPassword(auth, "sachin@example.com", "123456")
+      .then((value) => {
+        console.log(value);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const putdata = () => {
+    set(ref(db, "users/1"), {
+      id: 1,
+      age: 20,
+      username: "sachin sharma",
+      email: "sachin@example.com",
+    });
+  };
+  return (
+    <div>
+      <button onClick={putdata}>Put Data</button>
+      <br />
+      <button onClick={handleSignup}>Sign Up</button>
+    </div>
+  )
+}
+
+export default Adding
---
+++ src/pages/signin.jsx
@@ -0,0 +1,46 @@
+import React,{useState }from 'react'
+import { getAuth, signInWithEmailAndPassword} from "firebase/auth";
+import { app } from "../firebase";
+const auth = getAuth(app);
+
+const Signin = () => {
+    const [email, setEmail] =useState("");
+    const [password, setPassword] =useState(""); 
+
+const handleSignin = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((value) => console.log("User signed in successfully!"))
+      .catch(
+        (err) => console.log("Error signing in!", error)    
+      );
+  };
+
+  return (
+    <div>
+      <form>
+       
+        <br />
+        <label >Email</label>
+        <input 
+        onChange={(e) => setEmail(e.target.value)}
+        value={email}
+        type="email" placeholder='Enter your email' />
+        <br />
+        <br />
+        <label >Password</label>
+        <input 
+        onChange={(e) => setPassword(e.target.value)}
+        value={password}
+        type="password" 
+        required
+        placeholder='Enter your password' />
+        <br />
+        <br />
+        <button onClick={handleSignin}>Sign In</button>
+      </form>
+    </div>
+  )
+
+};
+
+export default Signin 
